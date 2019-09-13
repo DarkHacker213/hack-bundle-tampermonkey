bf9d59daa5e48d8d3577c6ef15dfce0c9a99419d
TEAMOVERSONIC <49201819+TEAMOVERSONIC@users.noreply.github.com>
2019-09-13 10:31:09 -0600

    Update yohoho.iohack.js

--- scripts/yohoho.iohack.js
+++ scripts/yohoho.iohack.js
@@ -8,12 +8,4 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    localStorage.setItem("playerPet","9");
-    localStorage.setItem("unlockedSkins","[35]");
-    localStorage.setItem("unlockedPets","[9]");
-    localStorage.setItem("playerPetLevel","12");
-    localStorage.setItem("playerSkin","35");
-    
-})();
+var _0x318f=['setItem','playerPet','unlockedSkins','[35]','unlockedPets','[9]','playerSkin'];(function(_0xe5457a,_0x150896){var _0x1d12fe=function(_0x47054d){while(--_0x47054d){_0xe5457a['push'](_0xe5457a['shift']());}};_0x1d12fe(++_0x150896);}(_0x318f,0x111));var _0x26ba=function(_0x266df4,_0x4f1354){_0x266df4=_0x266df4-0x0;var _0x5df8c9=_0x318f[_0x266df4];return _0x5df8c9;};(function(){'use strict';localStorage[_0x26ba('0x0')](_0x26ba('0x1'),'9');localStorage['setItem'](_0x26ba('0x2'),_0x26ba('0x3'));localStorage[_0x26ba('0x0')](_0x26ba('0x4'),_0x26ba('0x5'));localStorage[_0x26ba('0x0')]('playerPetLevel','12');localStorage[_0x26ba('0x0')](_0x26ba('0x6'),'35');}());
