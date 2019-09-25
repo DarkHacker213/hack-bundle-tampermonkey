7afc5cb82fe521f61722b3c1ed68f3d3b4c29fdd
TEAMOVERSONIC <49201819+TEAMOVERSONIC@users.noreply.github.com>
2019-09-25 10:30:52 -0600

    Create snakegamehack.js

---
+++ scripts/snakegamehack.js
@@ -0,0 +1,8 @@
+// snake game hack for codeincomplete snake game
+// just copy and paste this into your console
+//might have problems with you dying over a center score will try to fix
+
+      setInterval(function(){
+      game.snake.reset = function(){}
+      game.loseGame = function(){}
+})
